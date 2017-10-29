9624a43226515749d2fc30cebbb134522167b369
M.Mucahid Benlioglu <mbenlioglu@sabanciuniv.edu>
2017-10-29 19:35:57 +0300

    Data crawl & README.md:   + Added Makefile-data to crawl test dataset from internet (for Linux)   * Modified README.md to describe full project

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# parallel-distance-one-coloring
-Distance one coloring of graphs using parallelization techniques for performance improvements
+2# Parallelized Distance-1 Graph Coloring
+This program executes sequential and parallel versions of distance-1 graph coloring algorithm, outputs the execution
+times of sequential and parallel versions(with 1, 2, 4, 8, 16 threads) as well as the total number of color used in
+each execution and runs a correctness check to confirm no adjacent vertices has the same color.
+
+**Implemented by:**
+ * [M.Mucahid Benlioglu](https://github.com/mbenlioglu)
+
+
+## Getting Started
+There are two make files included this project. “Makefile-data” is to crawl the test data that can be used for testing
+and developement. (tests for current implementation done on these datasets). Running following command on terminal will
+download and extract the data into "./data/"
+
+    $ make -f Makefile-data
+
+Input data is in [MTX format](http://math.nist.gov/MatrixMarket/formats.html) and converted into [CRS format](http://netlib.org/linalg/html_templates/node91.html)
+for processing. 
+
+Another Makefile is also included to compile codes into executable in Linux environment (Use visual studio with /openmp flag
+for Windows). Executing following command under "./src" will create an executable named "coloring" which takes single input
+argument, which is the path to the dataset.
+
+    $ make
+
+The program will run sequential and parallel versions of the algorithm and output the execution time and number of colors used.
+
+
+## Algorithm & Implementation
+In this section, sequential and parallel version of the algorithm will be explained. Performance results of each algorithm can
+be found in results section.
+
+### Sequential Algorithm
+Since the size of the graphs are huge, the algorithm follows a greedy approach instead of dynamic programming, therefore
+total number of colors used in the graph may result in a suboptimal number. The algorithm implements backtracking search
+algorithm but instead of traversing all possibilities, in each step it selects the most suitable vertex and color without
+turning back to try other possibilities.
+
+Suitability metric for vertices is minimum remaining values (MRV), since we don’t have a limitation for maximum number of
+colors, definition for a vertex with MRV changes to a vertex that has the most constraints. As a tie-breaker among MRV
+variables, degree heuristic is used, which means the vertex that introduces the most constraints on the remaining vertices
+is chosen. Finally, the least constraining color is chosen (i.e. the one that introduces least number of new constraints to
+remaining vertices) as the color of the selected vertex.
+
+### Parallel Version
+Parallelized implementation uses a divide and conquer approach, meaning that the graph is divided into smaller subgraphs,
+which are colored separately and concurrently according to the sequential logic, then these sub graphs are combined to get
+the final answer.
+
+Since the subgraphs are colored independently, possible conflicts occur at the borders of the subgraphs, therefore each
+combination step has a conflict detection and fixing step to remove these conflicts.
+
+## Results
+The following table shows the executions times of implementations explained in the previous sections.
+
+![Performance Result Table](/res/perf_results.png)
--- src/coloring.cpp
+++ src/coloring.cpp
@@ -1,9 +1,15 @@
 /*
+* Created by: mbenlioglu, October 26, 2017
 *
+* This program executes sequential and parallel versions of distance-1 graph coloring algorithm, outputs the execution
+* times of sequential and parallel versions(with 1, 2, 4, 8, 16 threads) as well as the total number of color used in
+* each execution and runs a correctness check to confirm no adjacent vertices has the same color.
 *
 */
 
 #include <iostream>
+#include <iomanip>
+#include <algorithm>
 #include <omp.h>
 
 #ifdef __cplusplus
@@ -20,7 +26,62 @@ extern "C" {
 
 #define DEBUG
 
-void print_usage() {
+inline bool is_correct(etype *row, vtype *col, vtype nov, int colors[])
+{
+	for (size_t i = 0; i < nov; i++)
+	{
+		int c = colors[i];
+		int numAdj = row[i + 1] - row[i];
+		for (size_t j = 0; j < numAdj; j++)
+		{
+			if (colors[col[j]] == c)
+			{
+#ifdef DEBUG
+				std::cout << "Same color detected vertices " << i << " and " << col[j] << "\n";
+#endif // DEBUG
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+inline int num_of_colors(int colors[], int size)
+{
+	std::sort(colors, colors + size);
+
+	int last, num;
+	last = -1;
+	num = 0;
+
+	for (size_t i = 0; i < size; i++)
+	{
+		if (colors[i] != last)
+		{
+			last = colors[i];
+			num++;
+		}
+	}
+
+	return num;
+}
+
+void color_graph_seq(etype *row, vtype *col, vtype nov, int colors[])
+{
+	// create MRV array (0 for all vertices at the beginning)
+
+	// create most constraining vertex array (initially # of neighbours)
+
+	// least constraining value (i.e. most common d-2 color)
+}
+
+void color_graph_par(etype *row, vtype *col, vtype nov, int colors[])
+{
+	// TODO: implement this
+}
+
+void print_usage()
+{
 	std::cout << "./coloring <filename>\n";
 }
 
@@ -32,14 +93,74 @@ int main(int argc, char *argv[])
 	vwtype *vwghts;
 	vtype nov, source;
 
-	if (argc != 1)
+	// Graph reading
+	if (argc != 2)
 	{
 		print_usage();
 		return 1;
 	}
 
+	if (read_graph(argv[1], &row_ptr, &col_ind, &ewghts, &vwghts, &nov, 0) == -1)
+	{
+		std::cout << "error in graph read\n";
+		return 1;
+	}
+
+	// Process input
+	double startTime, endTime, execTimeSeq;
+	double execTimePar[5];
+
+	int colorCntSeq;
+	int colorCntPar[5];
+
+	int *colors = new int[nov];
+	std::fill_n(colors, nov, -1);
+
+	// Sequential
+	std::cout << "Starting sequential algorithm...";
+	startTime = omp_get_wtime();
+	color_graph_seq(row_ptr, col_ind, nov, colors);
+	endTime = omp_get_wtime();
+	std::cout << "ended\n";
+	execTimeSeq = endTime - startTime;
+	colorCntSeq = num_of_colors(colors, nov);
+#ifdef DEBUG
+	std::cout << "Running correctness check..." << is_correct(row_ptr, col_ind, nov, colors) ? "correct\n" : "wrong!\n";
+#endif // DEBUG
+	std::fill_n(colors, nov, -1); // reinitialize
+
+	// Parallel
+	for (size_t i = 0; i < 5; i++)
+	{
+		omp_set_num_threads(i);
+		std::cout << "Starting parallel algorithm with " << i << " threads...";
+		startTime = omp_get_wtime();
+		color_graph_par(row_ptr, col_ind, nov, colors);
+		endTime = omp_get_wtime();
+		std::cout << "ended\n";
+		execTimePar[i] = endTime - startTime;
+		colorCntPar[i] = num_of_colors(colors, nov);
+#ifdef DEBUG
+		std::cout << "Running correctness check..." << is_correct(row_ptr, col_ind, nov, colors) ? "correct\n" : "wrong!\n";
+#endif // DEBUG
+		std::fill_n(colors, nov, -1); // reinitialize
+	}
 
 
+	// Print results
+	printf("| %-15s | %-12s | %-12s | %-15s |\n", "Algorithm", "Thread Count", "# of Colors", "Time");
+	std::cout << std::setfill('-') << std::setw(65) << "-\n\n";
+	printf("| %-15s | %-12d | %-12d | %-12.8f s |\n", "Sequential", 1, colorCntSeq, execTimeSeq);
+	printf("| %-15s | %-12d | %-12d | %-12.8f s |\n", "Parallel", 1, colorCntPar[0], execTimePar[0]);
+	printf("| %-15s | %-12d | %-12d | %-12.8f s |\n", "Parallel", 2, colorCntPar[1], execTimePar[1]);
+	printf("| %-15s | %-12d | %-12d | %-12.8f s |\n", "Parallel", 4, colorCntPar[2], execTimePar[2]);
+	printf("| %-15s | %-12d | %-12d | %-12.8f s |\n", "Parallel", 8, colorCntPar[3], execTimePar[3]);
+	printf("| %-15s | %-12d | %-12d | %-12.8f s |\n", "Parallel", 16, colorCntPar[4], execTimePar[4]);
+
+
+	std::cout << "Press any key to continue...";
+	std::cin.get();
+	std::cin.ignore();
 
 	return 0;
 }
